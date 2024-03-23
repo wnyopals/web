951eb169784c14729f26f7c5f988bdb0dbcdf179
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-03-22 19:11:23 -0700

    [webserver] Added a github action that auto pushes when changes are detected on main. Added a dockerfile that builds a docker container

---
+++ backend/main.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
+// const indexRoutes = require('./routes/index');
+// const { environment } = require("./config");
+// const isProduction = environment === "production";
+const isProduction = true;
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+const corsOptions = {
+  origin: isProduction ? "https://wnyopals.onrender.com" : "http://localhost:5173",
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+if (isProduction) {
+  console.log("PRODUCTION")
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true,
+      },
+    })
+  );
+} else {
+  console.log("DEVELOPMENT")
+}
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// app.use('/', indexRoutes);
+
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
+
+module.exports = app;
