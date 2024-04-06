8bb1898bb8ef3f772187da1e5c141c43391fe4b1
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-04-05 20:56:50 -0700

    [frontend] modified cors for beta

--- frontend/src/store/features/apiSlice.ts
+++ frontend/src/store/features/apiSlice.ts
@@ -3,8 +3,8 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const listingsApi = createApi({
     reducerPath: "listingsAPI",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080/api"
-        // baseUrl: "https://web-main.onrender.com/api"
+        // baseUrl: "http://localhost:8080/api"
+        baseUrl: "https://web-main.onrender.com/api"
     }),
     endpoints: (builder) => ({
         getAllListings: builder.query({
