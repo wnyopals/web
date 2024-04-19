7aafd7d7b9172b07a66c2fec8bea5a41be82188c
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-04-19 12:54:36 -0700

    [#16] added some default seeder data

--- backend/db/seeders/20240329205141-Users.js
+++ backend/db/seeders/20240329205141-Users.js
@@ -1,25 +1,24 @@
 'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
+
+const bcrypt = require('bcryptjs')
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+    await queryInterface.bulkInsert("Users", [
+      {
+        password: String(bcrypt.hashSync("Password123!")),
+        email: "testabc123@test.com",
+        firstName: "Johnny",
+        lastName: "Test",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
