ec3986aeb9510dd6c1b708dc81f21e32ee0c8354
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-05-04 18:40:05 -0700

    [#17] Created user routes. This is not complete yet

---
+++ backend/routes/api/userRoutes.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const expressAsyncHandler = require("express-async-handler");
+const db = require("../../db/models");
+const { Op } = require("sequelize");
+const { validateAccessToken } = require("../../utils/auth")
+
+// Get user safe object
+// expect the req to include an id for the user
+router.get("/:id", expressAsyncHandler(async (req, res, next) => {
+    const {id} = req.params;
+    const user = await db.User.scope("currentUser").findByPk(id,
+        {
+            include: [
+            {
+                model: db.Inquerry
+            },
+            {
+                model: db.Transaction,
+                // include: [db.TransactionStatus, db.Listing]
+                include: [
+                    db.Listing
+                ]
+            }
+        ]
+    })
+    console.log(id);
+    if (!user) throw new Error("user doesn't exist")
+    res.json(user);
+}))
+
+// Update user
+//TODO: Need middleware to verify user
+router.put("/:id", validateAccessToken, expressAsyncHandler(async (req, res, next) => {
+    const {id} = req.params;
+    const user = await db.User.scope("currentUser").findByPk(id)
+}))
+
+
+// Delete user
+//TODO: Need middleware to verify user
+router.delete("/:id", validateAccessToken, expressAsyncHandler(async (req, res, next) => {
+    try{
+        const user = req.user
+        const userId = parseInt(req.params.id)
+
+        console.log(user)
+        
+        if (!user) throw new Error("User not found")
+        if (user.id === userId) {
+            try{
+                const userToDestroy = await db.User.findOne({
+                    where: {
+                        id: userId
+                    }
+                })
+                await userToDestroy.destroy();
+                res.json({
+                    message: "User deleted successfully"
+                })
+            } catch (e) {
+                next(e)
+            }
+        } else {
+            throw new Error(`Token is valid but user doesn't match. ${userId} != ${user.id}`)
+        }
+    } catch (e) {
+        next(e)
+    }
+}))
+
+module.exports = router;
