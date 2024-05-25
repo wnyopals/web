6ab09b44ba347e90cf0b9cd0a2c889cda3897424
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-05-25 10:41:46 -0700

    [noticket] fixed an issue where the backend couldn't find a file

--- backend/routes/api/messageRoutes.js
+++ backend/routes/api/messageRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const expressAsyncHandler = require("express-async-handler");
-// const db = require("../../db/models");
-const Inquiry = require("../../db/models/Inquiry");
-const Message = require("../../db/models/message");
+const db = require("../../db/models");
 const { Op, where } = require("sequelize");
 const { validateAccessToken } = require("../../utils/auth");
 
-router.post("")
+// router.post("")
 
 module.exports = router;
