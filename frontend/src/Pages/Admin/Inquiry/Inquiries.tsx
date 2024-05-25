46f8746d628fb6f1842151b7053484484a28fd79
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-05-25 10:30:42 -0700

    [#33 #26] updated code for inquiries page

--- frontend/src/Pages/Admin/Inquiry/Inquiries.tsx
+++ frontend/src/Pages/Admin/Inquiry/Inquiries.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import { useGetInquiriesQuery } from '../../../store/features/apiSlice'
+import React, { useEffect } from "react";
+import { useGetInquiriesQuery } from "../../../store/features/apiSlice";
 const Inquiries = () => {
-  const {data, status} = useGetInquiriesQuery();
+  const { data, isLoading, isSuccess, isError } = useGetInquiriesQuery();
+  let inquiries: React.ReactNode;
+  if (isLoading) {
+    inquiries = <h1>loading...</h1>;
+  } else if (isSuccess) {
+    inquiries = data?.map((item) => (
+      <div>
+        <h1>{item.Listing.title}</h1>
+        <h2>{item.subject}</h2>
+        <h3>{item.email}</h3>
+        <p>{item.subject}</p>
+      </div>
+    ));
+  } else if (isError) {
+    inquiries = <><h2>Error</h2><p>error message</p></>
+  }
+  useEffect(() => {
+    console.log(inquiries);
+    console.log(data)
+  });
   return (
     <div>
       <h1>Inquiries</h1>
       <div>
-        {data?.map(item => (
-          <>
-            <p>{item.Listing.title}</p>
-            <h2>{item.subject}</h2>
-          </>
-        ))}
+        {inquiries}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inquiries
+export default Inquiries;
