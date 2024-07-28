f400fd984439d4bfdcbe079e85bc476f2e0932cc
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-07-27 19:01:56 -0700

    removed custom state for stripe

--- frontend/src/store/cart.ts
+++ frontend/src/store/cart.ts
@@ -1,12 +1,9 @@
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { listing } from "../../types/Listing";
 import { loadCartState, saveCartState } from "../utils/localStorage";
-import { PurchaseInfo, PurchaseLookup, StartPurchaseResponse, UpdatePurchase} from "../../types/Purchases";
-import { PaymentIntent } from "@stripe/stripe-js";
 
 export type cartState = {
   cart: listing[];
-  clientSecret: string;
   amount: number;
 };
 
@@ -21,62 +18,6 @@ const initialState: statusCartState = {
   ...loadCartState(),
 };
 
-const apiUrl: string = "http://localhost:8080/api/payments"
-
-export const startPurchase = createAsyncThunk<StartPurchaseResponse, PurchaseInfo, {rejectValue: string}>(
-  "cart/startPurchase",
-  async (purchaseInfo) => {
-    if (!purchaseInfo) throw new Error("no purchase info provided");
-    const response = await fetch(apiUrl + "/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(purchaseInfo),
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    }
-  }
-);
-
-export const getPurchase = createAsyncThunk<PaymentIntent, PurchaseLookup, {rejectValue: string}>(
-  "cart/getPurchase", 
-  async (purchaseInfo) => {
-    if (!purchaseInfo) throw new Error("no purchase info provided");
-    const response = await fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(purchaseInfo)
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data
-    }
-  }
-)
-
-export const updatePurchase = createAsyncThunk<PaymentIntent, UpdatePurchase, {rejectValue: string}>(
-  "cart/updatePurchase",
-  async (purchaseInfo) => {
-    if (!purchaseInfo) throw new Error("no purchase info provided");
-    const response = await fetch(apiUrl, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(purchaseInfo)
-    });
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    }
-  }
-)
-
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -89,62 +30,24 @@ export const cartSlice = createSlice({
         alert("Item is already in cart");
         return;
       }
-
       state.cart.push(action.payload);
+      state.amount += action.payload.price
       saveCartState(state);
     },
     removeItem: (state, action) => {
       state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+      state.amount -= action.payload.price
       saveCartState(state);
     },
     setItems: (state, action) => {
       state.cart = action.payload;
     },
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(startPurchase.pending, (state) => {
-        state.status = "pending";
-        state.error= "";
-      })
-      .addCase(startPurchase.fulfilled, (state, action) => {
-        state.status = "fulfilled"
-        state.clientSecret = action.payload.client_secret
-      })
-      .addCase(startPurchase.rejected, (state, action) => {
-        state.status = "rejected";
-        state.error = action.error.message;
-      })
-      .addCase(getPurchase.pending, (state) => {
-        state.status = "pending";
-        state.error= "";
-      })
-      .addCase(getPurchase.fulfilled, (state, action) => {
-        state.status = "fulfilled"
-        state.clientSecret = action.payload.client_secret ?? ""
-        state.amount = action.payload.amount
-      })
-      .addCase(getPurchase.rejected, (state, action) => {
-        state.status = "rejected";
-        state.error = action.error.message;
-      })
-      .addCase(updatePurchase.pending, (state) => {
-        state.status = "pending";
-        state.error= "";
-      })
-      .addCase(updatePurchase.fulfilled, (state, action) => {
-        state.status = "fulfilled"
-        state.clientSecret = action.payload.client_secret ?? ""
-        state.amount = action.payload.amount
-      })
-      .addCase(updatePurchase.rejected, (state, action) => {
-        state.status = "rejected";
-        state.error = action.error.message;
-      });
-
+    setAmount: (state, action) => {
+      state.amount = action.payload
+    },
   },
 });
 
-export const { addItem, removeItem, setItems } = cartSlice.actions;
+export const { addItem, removeItem, setItems, setAmount } = cartSlice.actions;
 
 export default cartSlice.reducer;
