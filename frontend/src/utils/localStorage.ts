81320dcdb434a323f74b996717004d1ad86ede97
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-07-27 19:02:14 -0700

    reformatted code

--- frontend/src/utils/localStorage.ts
+++ frontend/src/utils/localStorage.ts
@@ -4,18 +4,18 @@ export function loadCartState(): cartState {
   try {
     const cart = localStorage.getItem("cart");
     console.log("Cart", cart)
-    if (cart === null) return {cart: [], clientSecret: "", amount: 0.00};
+    if (cart === null) return {cart: [], amount: 0.00};
     const parsedCartState: cartState = JSON.parse(cart);
     return parsedCartState;
   } catch (e) {
     console.error("Cart could not be loaded from local storage");
-    return {cart: [], clientSecret: "", amount: 0.00};
+    return {cart: [], amount: 0.00};
   }
 }
 
 export function saveCartState(state: cartState) {
   try {
-    const cart = JSON.stringify({cart: state.cart, clientSecret: state.clientSecret});
+    const cart = JSON.stringify({cart: state.cart, amount: state.amount});
     localStorage.setItem("cart", cart);
   } catch (e) {
     console.error("Could not save cart to local storage", e);
