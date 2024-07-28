78f05ff1c86241be437f568b365c70c44928f382
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-07-27 18:57:02 -0700

    Added Stripe payment to backend

--- backend/routes/api/payment.js
+++ backend/routes/api/payment.js
@@ -6,24 +6,42 @@ const {stripeApiKey} = require("../../config/index")
 const Stripe = require("stripe")
 const stripe = Stripe(stripeApiKey)
 
-router.post("/", expressAsyncHandler(async (req, res, next) => {
-    const {amount, currency} = req.body;
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount * 100, currency
+const YOUR_DOMAIN = "http://localhost:5173/payments";
+
+const endpointSecret = process.env.PRODUCTION ? "" : process.env.STRIPE_ENDPOINT_SECRET;
+
+
+router.post('/create-checkout-session', async (req, res) => {
+    const cart = req.body;
+    console.log(cart)
+    const lineItems = cart.map(item => ({
+        price_data: {
+            currency: "USD",
+            product_data: {
+                name: item.title
+            },
+            unit_amount: item.price*100
+        },
+        quantity: 1
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+      customer_email: 'customer@example.com',
+      submit_type: 'pay',
+      billing_address_collection: 'auto',
+      shipping_address_collection: {
+        allowed_countries: ['US'],
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}?success=true`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+      automatic_tax: {enabled: false},
     });
-    res.json({
-        client_secret: paymentIntent.client_secret,
-    });
-}))
+  
+    res.json(session)
+  });
+  
 
-router.put("/")
 
-router.post("/update-intent", expressAsyncHandler(async (req, res, next) => {
-    //This is just in case details on the payment change
-    //Addresses, card details, name, etc
-}))
-
-router.post("/confirm-intent", expressAsyncHandler(async (req, res, next) => {
-
-}))
 module.exports = router;
