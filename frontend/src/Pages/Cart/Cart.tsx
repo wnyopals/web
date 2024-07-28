45c0b838521c5fe5b3473b7ea8c0167469ffcdfa
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-07-27 19:00:31 -0700

    removed stripe checkout components

--- frontend/src/Pages/Cart/Cart.tsx
+++ frontend/src/Pages/Cart/Cart.tsx
@@ -1,21 +1,20 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
 
 import "./Cart.css"
-import { AddressElement, Elements, PaymentElement} from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { setAmount } from "../../store/cart";
+// import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const cart = useSelector((state: RootState) => state.cart.cart);
-  const subtotal: number = Math.round(cart.reduce<number>(
-    (sum, current) => sum + current.price,
-    0
-  ));
-  const stripePromise = loadStripe(`pk_test_51PLDstP6YFbHfJ6EmtPaRqVgEGcNRtx7fnYjiDUQSuTOGplgoM5H11LtpXlE1XbiSqH0O6E33i7xx8f1qJnswx7Y00S69kQ4Eb`);
+  const subtotal: number = useSelector((state: RootState) => state.cart.amount)
+  dispatch(setAmount(subtotal))
+
+
+
   return (
     <div className="cart-page">
-        <AddressElement options={{mode: 'billing'}}/>
-        <PaymentElement />
       <div className="cart-items">
         <h2>Cart</h2>
         {cart.map((item) => (
@@ -31,12 +30,9 @@ const Cart = () => {
             <h2>Subtotal</h2>
             <p>{subtotal}</p>
         </div>
-        {/* <div className="actions">
-            <button>Checkout now</button>
-        </div> */}
-        <Elements stripe={stripePromise}> 
-            <AddressElement options={{mode: "billing"}}/>
-        </Elements>
+        <div className="actions">
+          <button>Checkout</button>
+        </div>
       </div>
     </div>
   );
