1e52640d0061ba43600dd2821f420a08ebee3698
goldeneye5671 <59634572+goldeneye5671@users.noreply.github.com>
2024-07-29 10:23:14 -0700

    [noticket] added image slide show to cart and to shop page

--- frontend/src/components/ListingResult/ListingResult.tsx
+++ frontend/src/components/ListingResult/ListingResult.tsx
@@ -3,6 +3,7 @@ import "./ListingResult.css";
 import { listing } from "../../../types/Listing";
 import { useDeleteListingMutation } from "../../store/features/apiSlice";
 import { Link, NavLink } from "react-router-dom";
+import ImageSlideShow from "../ImageSlideShow/ImageSlideShow";
 
 const ListingResult: React.FC<{ listing: listing, admin: boolean }> = ({ listing, admin }) => {
   const [
@@ -31,7 +32,13 @@ const ListingResult: React.FC<{ listing: listing, admin: boolean }> = ({ listing
 
   return (
     <div className="listing-result">
-      <img />
+      <div>
+        <ImageSlideShow images={                [
+                  "https://i.ibb.co/yf6gYkm/F583-F9-CD-C3-B7-4803-A353-319-A3-F1-FB959-1-201-a.jpg",
+                  "https://i.ibb.co/Y0fxXvb/9-B5-A39-F3-43-F3-48-B9-8-F7-F-E312-B6403-EF1-1-201-a.jpg",
+                  "https://i.ibb.co/Nx7YMVB/5-A5-ED896-5317-46-D2-B6-B1-9-ECBE4-CE9535-1-201-a.jpg",
+                ]} imageHeight={150} imageWidth={150} />
+      </div>
       <h3 className="title">{listing?.title}</h3>
       <p className="description">{listing?.description}</p>
       <span className="quick-info">
